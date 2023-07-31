3ba532ed43733a47e5d655e2331d7133563269f9
Marija Kocevska <kocevska_m@hotmail.com>
2023-07-31 16:59:59 +0100

    board components

---
+++ app/boards/BoardCard.tsx
@@ -0,0 +1,13 @@
+import MiniWavePlayer from "../waves/MiniWavePlayer";
+
+const BoardCard: React.FC = () => {
+
+    return (
+        <section>
+            <p>Board</p>
+            <MiniWavePlayer/>
+        </section>
+    );
+};
+
+export default BoardCard;
---
+++ app/boards/BoardList.tsx
@@ -0,0 +1,12 @@
+import BoardCard from "./BoardCard";
+
+const BoardList: React.FC = () => {
+    return (
+
+        <section>
+            <BoardCard/>
+        </section>
+    );
+};
+
+export default BoardList;
