9560048eb2f0802bdaeae629f432f99489a8ce8f
Marija Kocevska <kocevska_m@hotmail.com>
2023-08-01 13:57:10 +0100

    followed component

---
+++ app/followed/page.tsx
@@ -0,0 +1,14 @@
+const FollowedBoards: React.FC = () => 
+{ 
+return (
+
+    <section>
+     <h2>Followed Boards</h2>
+   
+
+    </section>
+);
+
+};
+
+export default FollowedBoards;
--- app/waves/page.tsx
+++ app/waves/page.tsx
@@ -1,8 +1,25 @@
 import WaveList from "./WaveList";
+import {useEffect, useState} from "react"
+
+
+const [waves, setWaves] = useState([])
+
+useEffect(() => {
+  setWaves([
+    {
+      wave_id: 1,
+      title: " Harry Potter",
+      user: {username: "Moxy", avatar_url:""},
+
+
+    }
+  ])
+}, [])
+
 
 function Page() {
   return <section>
-  <h2 > All Waves</h2 >
+  <h2>All Waves</h2>
   <WaveList />
   </section>
 }
