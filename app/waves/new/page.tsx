e6d7e0befca510e3f5c83883bd57bc09ec865fad
adamsaunders26 <a.saunders206@gmail.com>
2023-08-03 14:30:41 +0100

    update url to hosted backend

--- app/waves/new/page.tsx
+++ app/waves/new/page.tsx
@@ -4,7 +4,7 @@ import { FormEvent, useEffect, useState } from "react";
 import TranscriptViewer from "./TranscriptViewer";
 import UploadAudio from "./UploadAudio";
 import VoiceRecorder from "./VoiceRecorder";
-import axios from 'axios';
+import axios from "axios";
 
 function Page() {
   const [audioData, setAudioData] = useState<Blob | null>(null);
@@ -13,28 +13,39 @@ function Page() {
     event.preventDefault();
 
     const formData = new FormData(event.target);
-    formData.append('audio_file', audioData, 'audio.webm');
+    formData.append("audio_file", audioData, "audio.webm");
 
-    axios.post('http://localhost:9091/api/waves',
-    formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      }
-    }).then((response) => {
-      console.log(response);
-    }).catch((err) => {
-      console.log(err);
-    });
+    axios
+      .post("https://back-end-sound-waves.onrender.com/api/waves", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        console.log("it worked!");
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log("it didn't work");
+        console.log(err);
+      });
   };
 
   return (
     <section>
       <h2>Create a new wave</h2>
       <VoiceRecorder setAudioData={setAudioData} />
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={(e) => {
+          console.log("submitting");
+          handleSubmit(e);
+        }}
+      >
         <input name="title" value="Test Wave Title" />
-        <input name="username" value="testuser" />
-        <input name="board_slug" value="board-slug" />
+        <input name="username" value="Big A" />
+        <input name="board_slug" value="stupid-films" />
+        <input name="created_at" value="2022-09-04T00:00:00Z" />
+
         <button>Submit</button>
       </form>
       <UploadAudio />
