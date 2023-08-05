c7d50076b5ea77f5f568dc53fc31e6549444791d
adamsaunders26 <a.saunders206@gmail.com>
2023-08-05 14:12:35 +0100

    render waveCards in pairs to allow for details in future

--- app/waves/WaveList.tsx
+++ app/waves/WaveList.tsx
@@ -7,10 +7,32 @@ interface Props {
 }
 
 function WaveList({ waves }: Props) {
-  return waves.map((wave) => (
-    // <WaveCard key={wave.wave_id} wave={wave} />
-    <TwoRowWaveCard key={wave.wave_id} wave={wave} />
-  ))
+  let wavePair = [];
+  const wavePairArray = [];
+  let startNewPair = true;
+  waves.forEach((wave) => {
+    if (startNewPair) {
+      wavePair.push(wave);
+    } else {
+      wavePair.push(wave);
+      wavePairArray.push(wavePair);
+      wavePair = [];
+    }
+    startNewPair = !startNewPair;
+  });
+  
+  return wavePairArray.map(([wave1, wave2]) => {
+    return (
+      <section className="grid grid-cols-2 sm:grid-cols-2 gap-4">
+        <TwoRowWaveCard key={wave1.wave_id} wave={wave1} />
+        <TwoRowWaveCard key={wave2.wave_id} wave={wave2} />
+      </section>
+    );
+  });
+  // return waves.map((wave) => (
+  //   // <WaveCard key={wave.wave_id} wave={wave} />
+  //   <TwoRowWaveCard key={wave.wave_id} wave={wave} />
+  // ));
 }
 
 export default WaveList;
