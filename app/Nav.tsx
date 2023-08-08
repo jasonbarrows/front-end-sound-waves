39c9d6b5e5c8b2b742ef8d9a19689ce9cad9d630
Jason Barrows <jasonbarrows@gmail.com>
2023-08-08 10:48:07 +0100

    remove unused markup from nav component

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -17,11 +17,6 @@ import {
   SquaresPlusIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
-import {
-  ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
-} from "@heroicons/react/20/solid";
 
 const products = [
   {
@@ -55,17 +50,9 @@ const products = [
     icon: ArrowPathIcon,
   },
 ];
-const callsToAction = [
-  // { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  // { name: 'Contact sales', href: '#', icon: PhoneIcon },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function Example() {
-  const { currentUser, setCurrentUser } = useContext<UserContent>(UserContext);
+  const { currentUser, setCurrentUser } = useContext(UserContext) as UserContent;
 
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -79,7 +66,6 @@ export default function Example() {
           <Link href="/waves" className="-m-1.5 p-1.5">
             <span className="sr-only">SoundWaves</span>
             <GiBigWave className="h-6 w-6" />
-            {/* <img className="h-6 w-6" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
           </Link>
         </div>
         <div>
@@ -97,20 +83,11 @@ export default function Example() {
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
-            {/* <Bars3Icon className="h-6 w-6" aria-hidden="true" /> */}
             <PiWaves className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
           <Popover className="relative">
-            {/* <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              Followed Boards
-              <ChevronDownIcon
-                className="h-5 w-5 flex-none text-gray-400"
-                aria-hidden="true"
-              />
-            </Popover.Button> */}
-
             <Transition
               as={Fragment}
               enter="transition ease-out duration-200"
@@ -146,21 +123,6 @@ export default function Example() {
                     </div>
                   ))}
                 </div>
-                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                  {callsToAction.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                    >
-                      <item.icon
-                        className="h-5 w-5 flex-none text-gray-400"
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
               </Popover.Panel>
             </Transition>
           </Popover>
@@ -189,8 +151,7 @@ export default function Example() {
             href="/users"
             className="text-sm font-semibold leading-6 text-gray-900"
           >
-            {/* {currentUser.hasOwnProperty(username) ? currentUser.username : 'username'} */}
-            <span>Aloha, {currentUser.username}</span>
+            <span>Aloha, {currentUser?.username}</span>
           </Link>
           <Link
             href="/login"
@@ -211,11 +172,6 @@ export default function Example() {
           <div className="flex items-center justify-between">
             <Link href="/waves" className="-m-1.5 p-1.5" onClick={() => { setMobileMenuOpen(false) }}>
               <span className="sr-only">SoundWaves</span>
-              {/* <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              /> */}
               <GiBigWave className="h-8 w-auto" />
             </Link>
             <button
@@ -230,34 +186,6 @@ export default function Example() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                {/* <Disclosure as="div" className="-mx-3">
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        Followed Boards
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? "rotate-180" : "",
-                            "h-5 w-5 flex-none"
-                          )}
-                          aria-hidden="true"
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure> */}
                 <Link
                   href="/waves"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
@@ -286,7 +214,6 @@ export default function Example() {
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   onClick={() => { setMobileMenuOpen(false) }}
                 >
-                  {/* {currentUser.hasOwnProperty(username) ? currentUser.username : 'username'} */}
                   Aloha, BigC
                 </Link>
                 <Link
