59c0d0a1259e348a857152a2402665cadb0e385c
Jason Barrows <jasonbarrows@gmail.com>
2023-08-10 12:34:17 +0100

    last minute styling changes

--- app/NowPlaying.tsx
+++ app/NowPlaying.tsx
@@ -11,13 +11,11 @@ const NowPlaying: React.FC = () => {
   } = useContext(WaveContext) as WaveContextType;
 
   return (
-    <div
-      className="flex items-center space-y-1 left-0 right-0 fixed mx-3 bottom-3 h-32 rounded-[2rem] justify-around"
-    >
-      <div className="py-2 p-4 w-full shadow-lg flex flex-col space-y-1 bg-white border-2 border-violet-700 rounded-[2rem]">
-        <div className="mx-3 flex items-baseline text-xs">
-          <span className="whitespace-nowrap">Now playing: </span>
-          <span className=" ml-2 text-sm font-medium text-violet-900 truncate">
+    <div className="flex items-center space-y-1 left-0 right-0 fixed -m-0.5 bottom-0 h-32 justify-around">
+      <div className="py-2 p-4 w-full shadow-lg flex flex-col space-y-1 bg-white border-t-2 border-b-0 border-pink-700">
+        <div className="mx-3 flex items-baseline text-sm">
+          <span className="whitespace-nowrap text-neutral-500">Now playing: </span>
+          <span className=" ml-2 font-medium text-violet-900 truncate">
             {
               currentWave
               ? <Link href={`/waves/${currentWave.wave_id}`}>{currentWave.title}</Link>
