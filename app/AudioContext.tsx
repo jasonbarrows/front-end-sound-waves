5de2db66e7be526d5fdc1d056c3afaeca16673e4
adamsaunders26 <a.saunders206@gmail.com>
2023-08-22 18:58:59 +0100

    fix typescript errors

--- app/AudioContext.tsx
+++ app/AudioContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, createContext, ReactNode, useRef } from "react";
+import { useState, createContext, ReactNode, useRef, ChangeEvent } from "react";
 import { Wave } from "./models";
 import PlayButton from "./PlayButton";
 import axios from "axios";
@@ -28,73 +28,106 @@ export const AudioProvider = ({ children }: { children: ReactNode }) => {
   const progressBar = useRef<HTMLInputElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const audioRef = useRef<HTMLAudioElement>(null);
-  const source = useRef<MediaElementAudioSourceNode>(null);
-  const analyser = useRef<AnalyserNode>(null);
+  const source = useRef<MediaElementAudioSourceNode | null>(null);
+  const analyser = useRef<AnalyserNode | null>(null);
 
   const calculateTime = (sec: number) => {
     const minutes = Math.floor(sec / 60);
     const seconds = Math.floor(sec % 60);
 
-    return `${minutes}:${('00' + seconds).slice(-2)}`;
-  }
+    return `${minutes}:${("00" + seconds).slice(-2)}`;
+  };
 
   const whilePlaying = () => {
     const time = Math.floor(audioRef.current?.currentTime as number);
-    (progressBar.current.value) = time; //audioRef.current.currentTime;
+    if (progressBar.current) {
+      progressBar.current.value = time.toString(); //audioRef.current.currentTime;
+    }
     setCurrentTime(time); //audioRef.current.currentTime);
     timeController = requestAnimationFrame(whilePlaying);
   };
 
-  const changeRange = (event) => {
-    audioRef.current.currentTime = event.target.value;
-    setCurrentTime(event.target.value);
+  const changeRange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = Number(event.target.value);
+    }
+    setCurrentTime(Number(event.target?.value));
   };
 
   const handlePlayAudio = () => {
     let audioContext = new AudioContext();
 
     if (!source.current) {
-      source.current = audioContext.createMediaElementSource(audioRef.current);
-      analyser.current = audioContext.createAnalyser();
-      source.current.connect(analyser.current);
-      analyser.current?.connect(audioContext.destination);
-      analyser.current.fftSize = 64;
+      if (audioRef.current) {
+        source.current = audioContext.createMediaElementSource(
+          audioRef.current
+        );
+        analyser.current = audioContext.createAnalyser();
+        source.current.connect(analyser.current);
+        analyser.current?.connect(audioContext.destination);
+        analyser.current.fftSize = 64;
+      }
     }
 
-    const bufferLength = analyser.current.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
+    const bufferLength = analyser.current?.frequencyBinCount;
+    let dataArray: unknown;
+    if (bufferLength) {
+      dataArray = new Uint8Array(bufferLength);
+    }
 
     // if (audioRef.current.paused) {
     //   setIsPlaying(false);
     //   return cancelAnimationFrame(animationController);
     // }
 
-    const ctx = canvasRef.current.getContext("2d");
-    const barWidth = canvasRef.current.width / bufferLength;
-    let barHeight;
+    const ctx = canvasRef.current?.getContext("2d");
+    let barWidth: unknown;
+    if (canvasRef.current && bufferLength) {
+      barWidth = canvasRef.current.width / bufferLength;
+    }
+    let barHeight: unknown;
     let x;
 
-    let gradient = ctx.createLinearGradient(
+    let gradient = ctx?.createLinearGradient(
       0,
       0,
-      canvasRef.current.width,
-      canvasRef.current.height,
+      canvasRef.current?.width as number,
+      canvasRef.current?.height as number
     );
 
-    gradient.addColorStop(0, "#0284c7");
-    gradient.addColorStop(0.5, "#7c3aed");
-    gradient.addColorStop(1.0, "#db2777");
+    gradient?.addColorStop(0, "#0284c7");
+    gradient?.addColorStop(0.5, "#7c3aed");
+    gradient?.addColorStop(1.0, "#db2777");
 
     function animate() {
       x = 1;
-      ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-      analyser.current.getByteFrequencyData(dataArray);
+      ctx?.clearRect(
+        0,
+        0,
+        canvasRef.current?.width as number,
+        canvasRef.current?.height as number
+      );
+      analyser.current?.getByteFrequencyData(dataArray as Uint8Array);
 
-      for (let i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i] / 256 * canvasRef.current.height;
-        ctx.fillStyle = gradient;
-        ctx.fillRect(x, canvasRef.current.height, barWidth, -barHeight);
-        x += (barWidth + 1);
+      for (let i = 0; i < (bufferLength as number); i++) {
+        const typedDataArray = dataArray as Uint8Array;
+        if (canvasRef.current?.height) {
+          barHeight = (typedDataArray[i] / 256) * canvasRef.current?.height;
+        }
+        if (ctx?.fillStyle) {
+          ctx.fillStyle = gradient as CanvasGradient;
+        }
+
+        if (ctx?.fillRect) {
+          const typedBarHeight = barHeight as number;
+          ctx.fillRect(
+            x,
+            canvasRef.current?.height as number,
+            barWidth as number,
+            -typedBarHeight
+          );
+        }
+        x += (barWidth as number) + 1;
       }
 
       animationController = requestAnimationFrame(animate);
@@ -109,27 +142,41 @@ export const AudioProvider = ({ children }: { children: ReactNode }) => {
   }
 
   function play(wave: Wave) {
-    if (wave.hasOwnProperty('wave_url') && currentWave?.wave_url !== wave.wave_url) {
+    if (
+      wave.hasOwnProperty("wave_url") &&
+      currentWave?.wave_url !== wave.wave_url
+    ) {
       setCurrentWave(wave);
-      axios.get(`https://mffyiqvrkwogdmivjovi.supabase.co/storage/v1/object/public/waves/${wave.wave_url}`, {
-        responseType: 'blob',
-      }).then(({ data }) => {
-        audioRef.current.src = URL.createObjectURL(data);
-        return data.arrayBuffer();
-      }).then((arrayBuffer) => {
-        const audioCtx = new AudioContext();
-        return audioCtx.decodeAudioData(arrayBuffer);
-      }).then((data) => {
-        const seconds = Math.floor(data.duration);
-        setDuration(seconds);
-        progressBar.current.max = seconds;
-        audioRef.current?.play();
+      axios
+        .get(
+          `https://mffyiqvrkwogdmivjovi.supabase.co/storage/v1/object/public/waves/${wave.wave_url}`,
+          {
+            responseType: "blob",
+          }
+        )
+        .then(({ data }) => {
+          if (audioRef.current) {
+            audioRef.current.src = URL.createObjectURL(data);
+          }
+          return data.arrayBuffer();
+        })
+        .then((arrayBuffer) => {
+          const audioCtx = new AudioContext();
+          return audioCtx.decodeAudioData(arrayBuffer);
+        })
+        .then((data) => {
+          const seconds = Math.floor(data.duration);
+          setDuration(seconds);
+          if (progressBar.current) {
+            progressBar.current.max = seconds.toString();
+          }
+          audioRef.current?.play();
 
-        audioRef.current?.addEventListener('ended', () => {
-          setIsPlaying(false);
-          cancelAnimationFrame(animationController);
+          audioRef.current?.addEventListener("ended", () => {
+            setIsPlaying(false);
+            cancelAnimationFrame(animationController);
+          });
         });
-      });
     } else {
       audioRef.current?.play();
     }
@@ -150,14 +197,23 @@ export const AudioProvider = ({ children }: { children: ReactNode }) => {
     } else {
       play(wave);
     }
-  }
+  };
 
   const nowPlaying = () => (
     <>
       <audio ref={audioRef} />
-      <canvas ref={canvasRef} width={320} height={40} className="border border-neutral-300 rounded-md  shadow-md box-content" />
+      <canvas
+        ref={canvasRef}
+        width={320}
+        height={40}
+        className="border border-neutral-300 rounded-md  shadow-md box-content"
+      />
       <div className="mt-2 px-2 w-80 flex items-center justify-between space-x-2">
-      <PlayButton wave={currentWave} isPlaying={isPlaying} togglePlay={togglePlay} />
+        <PlayButton
+          wave={currentWave}
+          isPlaying={isPlaying}
+          togglePlay={togglePlay}
+        />
         <div className="font-mono text-xs">{calculateTime(currentTime)}</div>
         <div className="relative w-full flex-1 flex items-center">
           <input
